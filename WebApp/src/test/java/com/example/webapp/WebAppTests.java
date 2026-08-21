d1fc8e7c08d8a23c15a5010dd1f68d19f9c4feb1
rye <rye>
2026-08-21 14:35:13 -0400

    Scaffold WebApp Spring Boot project

---
+++ WebApp/src/test/java/com/example/webapp/WebAppTests.java
@@ -0,0 +1,14 @@
+package com.example.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class WebAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
